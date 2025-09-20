7d77055ff8e19f648489f7b75574bea9a01f5916
Venomous10monish <monishparmar55@gmail.com>
2025-09-20 19:47:24 +0530

    update

--- server.js
+++ server.js
@@ -15,7 +15,12 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://monish-parmar10.github.io",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
